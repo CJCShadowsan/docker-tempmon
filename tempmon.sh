d50d0a5e8aa77556b01fdaa91c8b44218d0adbe6
Chris Coates <christopher.james.coates@gmail.com>
2022-07-30 16:30:02 +0100

    Update tempmon.sh

--- tempmon.sh
+++ tempmon.sh
@@ -3,21 +3,20 @@
 #  exceeds a particular value.
 #  80ºC is the maximum allowed for a Raspberry Pi.
 
-# Get the reading from the sensor and strip the non-number parts
-SENSOR="`/opt/vc/bin/vcgencmd measure_temp | cut -d "=" -f2 | cut -d "'" -f1`"
+# Get the reading from the Ambient sensor and strip the non-number parts
+SENSOR="/usr/sbin/ipmitool sdr elist | grep Ambient | awk '{print $(NF-2)}'"
+#SENSOR="`/opt/vc/bin/vcgencmd measure_temp | cut -d "=" -f2 | cut -d "'" -f1`"
 # -gt only deals with whole numbers, so round it.
 TEMP="`/usr/bin/printf "%.0f\n" ${SENSOR}`"
-# How hot will we allow the SoC to get?
-MAX="60"
+# How hot will we allow Ambient to get?
+MAX="30"
 
 if [ "${TEMP}" -gt "${MAX}" ] ; then
  # This will be mailed to root if called from cron
  echo "${TEMP}ºC is too hot!"
  # Send a message to syslog
- /usr/bin/logger "Shutting down due to SoC temp ${TEMP}."
+ /usr/bin/logger "Shutting down due to Ambient temp ${TEMP}."
  # Halt the box
- pkill fbi
- pkill kodi
  sudo /sbin/shutdown -h now
  echo "Initiating shutdown..."
  else
